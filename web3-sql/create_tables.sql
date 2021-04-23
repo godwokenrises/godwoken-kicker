bf215f13b55c0a09d4981263aea8c637f04530b7
RetricSu <inneverland2013@gmail.com>
2021-04-23 09:40:26 +0200

    pre-set table in postgres

---
+++ web3-sql/create_tables.sql
@@ -0,0 +1,65 @@
+
+-- Add migration script here
+CREATE TABLE blocks (
+    number NUMERIC PRIMARY KEY,
+    hash TEXT UNIQUE NOT NULL,
+    parent_hash TEXT NOT NULL,
+    logs_bloom TEXT NOT NULL,
+    gas_limit NUMERIC NOT NULL,
+    gas_used NUMERIC NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    miner TEXT NOT NULL,
+    size NUMERIC NOT NULL
+);
+
+
+-- Add migration script here
+CREATE TABLE transactions (
+    id BIGSERIAL PRIMARY KEY,
+    hash TEXT UNIQUE NOT NULL,
+    block_number NUMERIC REFERENCES blocks(number) NOT NULL,
+    block_hash TEXT NOT NULL,
+    transaction_index INTEGER NOT NULL,
+    from_address TEXT NOT NULL,
+    to_address TEXT,
+    value NUMERIC NOT NULL,
+    nonce NUMERIC,
+    gas_limit NUMERIC,
+    gas_price NUMERIC,
+    input TEXT,
+    v TEXT NOT NULL,
+    r TEXT NOT NULL,
+    s TEXT NOT NULL,
+    cumulative_gas_used NUMERIC,
+    gas_used NUMERIC,
+    logs_bloom TEXT NOT NULL,
+    contract_address TEXT,
+    status BOOLEAN NOT NULL
+);
+
+CREATE INDEX ON transactions (block_number);
+CREATE INDEX ON transactions (block_hash);
+CREATE INDEX ON transactions (from_address);
+CREATE INDEX ON transactions (to_address);
+CREATE INDEX ON transactions (contract_address);
+CREATE UNIQUE INDEX block_number_transaction_index_idx ON transactions (block_number, transaction_index);
+CREATE UNIQUE INDEX block_hash_transaction_index_idx ON transactions (block_hash, transaction_index);
+
+
+-- Add migration script here
+CREATE TABLE logs (
+    id BIGSERIAL PRIMARY KEY,
+    transaction_id BIGSERIAL REFERENCES transactions(id) NOT NULL,
+    transaction_hash TEXT NOT NULL,
+    transaction_index INTEGER NOT NULL,
+    block_number NUMERIC REFERENCES blocks(number) NOT NULL,
+    block_hash TEXT NOT NULL,
+    address TEXT NOT NULL,
+    data TEXT NOT NULL,
+    log_index INTEGER NOT NULL,
+    topics TEXT[] NOT NULL
+);
+
+CREATE INDEX ON logs (transaction_hash);
+CREATE INDEX ON logs (block_hash);
+CREATE INDEX ON logs (address);
