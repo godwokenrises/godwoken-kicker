7ed237768ca11f06000ff831ceeb3a5a5fe244a8
retricsu <inneverland2013@gmail.com>
2021-06-10 05:13:24 +0200

    refactor godwoken_entrypoint.sh

--- godwoken_entrypoint.sh
+++ godwoken_entrypoint.sh
@@ -23,27 +23,6 @@ fi
 # import some helper function
 source ${PROJECT_DIR}/gw_util.sh
 
-wait_for_polyman_prepare_rpc "$POLYMAN_RPC"
-
-# wait for godwoken-polyman deploy layer1 sudt script before starting godwoken
-callPolyman prepare_sudt_scripts "$POLYMAN_RPC"
-
-# save lumos-config file in godwoken folder
-callPolyman get_lumos_config "$POLYMAN_RPC"
-echo $call_result > $LUMOS_CONFIG_FILE
-sed -i -e 's/{"status":"ok","data"://g' $LUMOS_CONFIG_FILE
-sed -i -e 's/}}}}/}}}/g' $LUMOS_CONFIG_FILE
-
-cd ${PROJECT_DIR}
-# update l1_sudt_script_hash in config.toml file(if it exits) with lumos script.sudt.code_hash
-codeHash=$(get_lumos_config_script_key_value SUDT CODE_HASH "$LUMOS_CONFIG_FILE")
-set_key_value_in_toml "l1_sudt_script_type_hash" $codeHash "$GODWOKEN_CONFIG_TOML_FILE"
-# update l1_sudt_dep info in config.toml file(if it exits) with lumos script.sudt.dep
-depType=$(get_lumos_config_script_key_value SUDT DEP_TYPE "$LUMOS_CONFIG_FILE")
-txHash=$(get_lumos_config_script_key_value SUDT TX_HASH "$LUMOS_CONFIG_FILE")
-outpointIndex=$(get_lumos_config_script_key_value SUDT INDEX "$LUMOS_CONFIG_FILE")
-update_godwoken_config_toml_with_l1_sudt_dep "$GODWOKEN_CONFIG_TOML_FILE" $depType $txHash $outpointIndex
-
 # ready to start godwoken
 cd ${PROJECT_DIR}/godwoken
 
@@ -72,21 +51,29 @@ else
   export START_MODE="fat_start"
 fi
 
-
 if [ $START_MODE = "slim_start" ]; then
   RUST_LOG=gw_block_producer=info,gw_generator=debug $GODWOKEN_BIN
 else
   echo 'run deploy mode'
 fi
 
+# ========= below: run godwoken in deploy mode ============
+
+# === check and prepare for l1-sudt-script
+wait_for_polyman_prepare_rpc "$POLYMAN_RPC"
+callPolyman prepare_sudt_scripts "$POLYMAN_RPC"
+# save lumos-config file with new l1-sudt-script config in godwoken folder
+callPolyman get_lumos_config "$POLYMAN_RPC"
+echo $call_result > $LUMOS_CONFIG_FILE
+sed -i -e 's/{"status":"ok","data"://g' $LUMOS_CONFIG_FILE
+sed -i -e 's/}}}}/}}}/g' $LUMOS_CONFIG_FILE
+# update l1_sudt_script_hash in rollup-config.json file(if it exits) with lumos script.sudt.code_hash
+codeHash=$(get_lumos_config_script_key_value SUDT CODE_HASH "$LUMOS_CONFIG_FILE")
+set_key_value_in_json "l1_sudt_script_type_hash" $codeHash "deploy/rollup-config.json"
 
 # deploy scripts
 echo 'this may takes a little bit of time, please wait...'
 $GW_TOOLS_BIN deploy-scripts -r ${CKB_RPC} -i deploy/scripts-deploy.json -o deploy/scripts-deploy-result.json -k ${PRIVKEY}
-
-# update l1_sudt_script_hash in rollup-config.json file(if it exits) with lumos script.sudt.code_hash
-codeHash=$(get_lumos_config_script_key_value SUDT CODE_HASH "$LUMOS_CONFIG_FILE")
-set_key_value_in_json "l1_sudt_script_type_hash" $codeHash "deploy/rollup-config.json"
 
 # deploy genesis block
 $GW_TOOLS_BIN deploy-genesis -r ${CKB_RPC} -d deploy/scripts-deploy-result.json -p deploy/poa-config.json -u deploy/rollup-config.json -o deploy/genesis-deploy-result.json -k ${PRIVKEY}
