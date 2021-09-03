4ca306edde6c4e4e6fe4c257a5234e861112218c
haxyz <88162891+haxyz@users.noreply.github.com>
2021-09-03 16:16:37 +0100

    Fix docker group permissions

--- godwoken-kicker-setup.sh
+++ godwoken-kicker-setup.sh
@@ -14,8 +14,15 @@ sudo apt-get update;
 sudo apt-get install -y build-essential wget git p7zip-full python nodejs yarn;
 
 curl -fsSL https://get.docker.com | sudo  bash -;
-docker --version;
+sudo groupadd docker || true;
+sudo gpasswd -a $USER docker;
 
+echo "To use docker log out and log back in so that your group membership is re-evaluated";
+echo "or use just use: 'newgrp docker' to start a subshell with a re-evaluated group membership";
+
+newgrp docker << END
+set -eux;
+docker ps;
 sudo rm /usr/bin/docker-compose || true;
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 sudo chmod +x /usr/local/bin/docker-compose;
@@ -28,3 +35,4 @@ git clone https://github.com/RetricSu/godwoken-kicker.git;
 cd godwoken-kicker;
 make init;
 make start;
+END
