da6734a90b7dc56ef0173795471b1f3a4a674b3b
keroro <keroroxx520@gmail.com>
2022-03-09 19:03:17 +0800

    wip

--- gw_util.sh
+++ gw_util.sh
@@ -1037,23 +1037,24 @@ start() {
 # 1. deposit for block_producer
 # 2. deposit for meta_user as the eth_address_registor
 deposit_for_two_dev_accounts() {    
-    echo ">>> Use gw-tools to deposit for 2 dev accounts in ckb/specs/dev.toml"
+    echo "pass"
+    # echo ">>> Use gw-tools to deposit for 2 dev accounts in ckb/specs/dev.toml"
 
-    cd docker
-    docker-compose --env-file .build.mode.env exec -T -w/code/workspace godwoken bash -c "
-        bin/gw-tools deposit-ckb \
-        --privkey-path deploy/private_key \
-        --ckb-rpc http://ckb:8114 \
-        --scripts-deployment-path deploy/scripts-deploy-result.json \
-        --godwoken-rpc-url http://localhost:8119 \
-        --config-path config.toml \
-        --capacity 10000 &
-        bin/gw-tools deposit-ckb \
-        --privkey-path deploy/meta_user_private_key \
-        --ckb-rpc http://ckb:8114 \
-        --scripts-deployment-path deploy/scripts-deploy-result.json \
-        --godwoken-rpc-url http://localhost:8119 \
-        --config-path config.toml \
-        --capacity 10000
-    "
+    # cd docker
+    # docker-compose --env-file .build.mode.env exec -T -w/code/workspace godwoken bash -c "
+    #     bin/gw-tools deposit-ckb \
+    #     --privkey-path deploy/private_key \
+    #     --ckb-rpc http://ckb:8114 \
+    #     --scripts-deployment-path deploy/scripts-deploy-result.json \
+    #     --godwoken-rpc-url http://localhost:8119 \
+    #     --config-path config.toml \
+    #     --capacity 10000 &
+    #     bin/gw-tools deposit-ckb \
+    #     --privkey-path deploy/meta_user_private_key \
+    #     --ckb-rpc http://ckb:8114 \
+    #     --scripts-deployment-path deploy/scripts-deploy-result.json \
+    #     --godwoken-rpc-url http://localhost:8119 \
+    #     --config-path config.toml \
+    #     --capacity 10000
+    # "
 }
