757e7a2c4b0d96948c770661ee97baaf43ea8ebc
keroro <keroroxx520@gmail.com>
2022-05-07 10:08:36 +0800

    fix: remove manual-artifacts directory

--- docker/web3/entrypoint.sh
+++ docker/web3/entrypoint.sh
@@ -11,7 +11,5 @@ cp $CONFIG_DIR/web3-config.env $WORKSPACE/packages/api-server/.env
 # create folder for address mapping store
 mkdir -p /usr/local/godwoken-web3/address-mapping
 
-# start web3 server
-cd $WORKSPACE/packages/api-server 
-yarn start
-# NODE_ENV=production DEBUG=godwoken-web3-api:server pm2 start ./bin/cluster --no-daemon --name gw-web3 --max-memory-restart 1G
+cd $WORKSPACE
+yarn run start
