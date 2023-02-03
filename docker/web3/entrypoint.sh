452b728e0c780876f2d26e73c5f982922cbdfda5
RetricSu <inneverland2013@gmail.com>
2023-02-03 17:02:09 +0800

    fix(web3): entrypoint and docker workdir

--- docker/web3/entrypoint.sh
+++ docker/web3/entrypoint.sh
@@ -3,11 +3,5 @@
 set -o errexit
 #set -o xtrace
 
-# create folder for address mapping store
-mkdir -p /usr/local/godwoken-web3/address-mapping
-
-# WORKSPACE=/godwoken-web3
-cd /godwoken-web3
-
 yarn knex migrate:latest
 yarn run start:prod
